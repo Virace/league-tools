886e87c587442dcbe9c1dfef910ea52e90e54c89
Virace <Virace@aliyun.com>
2021-03-02 04:24:32 +0800

    wpk parse

---
+++ WPK/WPK.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/3/2 0:57
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: PyCharm
+# @Detail  :
+
+from base import SectionNoId, WemFile
+
+
+class WPK(SectionNoId):
+    __slots__ = [
+        'version',
+        'file_count',
+        'offsets',
+        'files'
+    ]
+
+    def _read(self):
+        head = self._data.customize('4s')
+        assert head == b'r3d2', f'WPK文件头错误. {head}'
+        self.files = []
+        self.version = self._data.customize('<L')
+        self.file_count = self._data.customize('<L')
+        self.offsets = self._data.customize(f'<{self.file_count}L', False)
+
+        for i in range(self.file_count):
+            self._data.seek(self.offsets[i], 0)
+            offset, length, filename_size = self._data.customize('<LLL', False)
+
+            # 字符串中间有空字节
+            # filename = self._data.str(filename_size * 2)
+            filename = bytearray(self._data.customize(f'<{filename_size}H', False)).decode('utf-8')
+
+            self._data.seek(offset, 0)
+            data = self._data.bytes(length)
+
+            self.files.append(
+                WemFile(
+                    filename=filename,
+                    data=data,
+                    offset=offset,
+                    length=length,
+                    id=int(filename.split('.')[0])
+                )
+            )
+
+    def __repr__(self):
+        return f'File_Version: {self.version}, ' \
+               f'Audio_Resources_Amount: {self.file_count}'
---
+++ WPK/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/3/2 0:15
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: PyCharm
+# @Detail  :
+
+from .WPK import WPK
+
+__all__ = [
+    'WPK'
+]
