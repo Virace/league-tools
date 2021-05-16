4a1ee28e778d8483e6554f8c2bbaf10734af6e31
Virace <Virace@aliyun.com>
2021-05-16 16:12:16 +0800

    update version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 # @Site    : x-item.com
 # @Software: Pycharm
 # @Create  : 2021/3/4 18:48
-# @Update  : 2021/5/16 15:56
+# @Update  : 2021/5/16 16:11
 # @Detail  : 
 
 import setuptools
 
-version = '1.0.0a6'
+version = '1.0.0a8'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -37,4 +37,3 @@ setuptools.setup(
     python_requires=">=3.8",
 
 )
-
