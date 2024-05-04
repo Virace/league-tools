fb3a20c5c3749c5f215b72c9b523fbff99e64948
Virace <Virace@aliyun.com>
2024-05-05 02:53:11 +0800

    typing

---
+++ league_tools/utils/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: Pycharm
+# @Create  : 2024/5/5 2:16
+# @Update  : 2024/5/5 2:16
+# @Detail  : 
---
+++ league_tools/utils/type_hints.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: Pycharm
+# @Create  : 2024/3/12 12:39
+# @Update  : 2024/5/5 2:18
+# @Detail  : 
+
+from typing import Union
+
+StrPath = Union[str, 'os.PathLike[str]']
