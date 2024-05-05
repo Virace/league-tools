7a4bedf25b19b55979aaa311d3012721e68ec669
Virace <Virace@aliyun.com>
2024-05-06 00:51:40 +0800

    移除日志处理

--- league_tools/__init__.py
+++ league_tools/__init__.py
@@ -4,13 +4,14 @@
 # @Site    : x-item.com
 # @Software: PyCharm
 # @Create  : 2021/3/4 18:44
-# @Update  : 2024/5/5 23:25
+# @Update  : 2024/5/6 0:20
 # @Detail  : 
 
 from loguru import logger
 from league_tools.index import extract_audio, get_event_hashtable, get_audio_files, get_audio_hashtable, extract_not_classified
 
 logger.disable("league_tools")
+logger.remove()
 
 __all__ = [
     'extract_audio',
@@ -19,3 +20,4 @@ __all__ = [
     'get_event_hashtable',
     'get_audio_hashtable'
 ]
+
