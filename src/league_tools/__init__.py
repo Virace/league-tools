c55979d1249656bf7ea648d6861e201241060838
Virace <Virace@aliyun.com>
2025-07-27 01:38:54 +0800

    移除不再使用的index.py、audio/__init__.py和audio/hash.py文件，更新__init__.py以包含新的导入和模块，提升代码结构和可维护性。

--- src/league_tools/__init__.py
+++ src/league_tools/__init__.py
@@ -1,12 +1,18 @@
-# -*- coding: utf-8 -*-
+# 🐍 Although that way may not be obvious at first unless you're Dutch.
+# 🐼 尽管这方法一开始并非如此直观，除非你是荷兰人
 # @Author  : Virace
 # @Email   : Virace@aliyun.com
 # @Site    : x-item.com
 # @Software: PyCharm
 # @Create  : 2021/3/4 18:44
-# @Update  : 2025/7/17 8:20
+# @Update  : 2025/7/27 1:38
 # @Detail  : 
 
-__all__ = [ ]
 
+from .formats.bin.parser import BIN
+from .formats.bnk.parser import BNK
+from .formats.wad.parser import WAD
+from .formats.wpk.parser import WPK
+from .utils.wwiser import WwiserManager
 
+__all__ = ["BIN", "BNK", "WPK", "WAD", "WwiserManager"]
